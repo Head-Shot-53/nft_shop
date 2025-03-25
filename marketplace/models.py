63bdfb6b859eaf2cb356fbc1bdbec9d0f3700426
Head-Shot-53 <denchikmashtack@gmail.com>
2025-03-25 19:07:54 +0200

    додав модель для патерну state(стани для товарів)

--- marketplace/models.py
+++ marketplace/models.py
@@ -10,18 +10,18 @@ class DigitalArtwork(models.Model):
         ('other', 'Інше')
     ]
 
-    # STATUS_CHOICES = [
-    #     ('available', 'Доступно'),
-    #     ('sold', 'Продано'),
-    #     ('hiden', 'Приховано'),
-    # ]
+    STATUS_CHOICES = [
+        ('available', 'Доступно'),
+        ('sold', 'Продано'),
+        ('hiden', 'Приховано'),
+    ]
 
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     art_type = models.CharField(max_length=20, choices=ART_TYPE_CHOICES)
     file = models.FileField(upload_to='artworks/')
-    # status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
