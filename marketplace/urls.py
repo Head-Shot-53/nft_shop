140f1429460a4fc95290fd2a23179b6f1525a85f
Head-Shot-53 <denchikmashtack@gmail.com>
2025-03-26 16:21:58 +0200

    додав шлях до головної сторінки

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.artwork_list, name='artwork_list')
+    path('artworks_list/',views.artwork_list, name='artwork_list'),
+    path('my-artworks/', views.my_artworks, name='my_artworks'),
+    path('', views.home, name='home')
 ]
