4404931dc42cb7d82551322d90143bf0f38a172f
Head-Shot-53 <denchikmashtack@gmail.com>
2025-03-26 21:34:41 +0200

    створив форму для додавання нових робіт автором

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -5,4 +5,8 @@ class ArtWorksStatusForms(forms.ModelForm):
     class Meta:
         model = DigitalArtwork
         fields = ['status']
-        
+
+class ArtWorkCreateForm(forms.ModelForm):
+    class Meta:
+        model = DigitalArtwork
+        fields = ['title', 'description', 'price', 'art_type', 'file', 'status']
