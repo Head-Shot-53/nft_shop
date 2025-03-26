7d6f4a131fb140fdd017a371b617e69c468a5dbc
Head-Shot-53 <denchikmashtack@gmail.com>
2025-03-26 21:36:46 +0200

    зробив маршрутизацію для створення, видалення та редагування

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('artworks_list/',views.artwork_list, name='artwork_list'),
     path('my-artworks/', views.my_artworks, name='my_artworks'),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('create-artwork/', views.create_artwork, name='create_artwork'),
+    path('artwork/<int:artwork_id>/', views.artwork_detail, name='artwork_detail'),
+    path('artwork/<int:artwork_id>/delete/', views.delete_artwork, name='delete_artwork'),
+    path('artwork/<int:artwork_id>/edit/', views.edit_artwork, name='edit_artwork'),
 ]
