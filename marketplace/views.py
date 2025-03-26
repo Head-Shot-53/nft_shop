a9792ed845d7a01a3d93ec25507c495d455acb05
Head-Shot-53 <denchikmashtack@gmail.com>
2025-03-26 21:35:51 +0200

    додав функції редагування арта, видалення та створення

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import DigitalArtwork
-from .forms import ArtWorksStatusForms
+from .forms import ArtWorksStatusForms, ArtWorkCreateForm
 from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, 'marketplace/home.html')
 
 def artwork_list(request):
     artworks = DigitalArtwork.objects.filter(status = 'available')
@@ -27,5 +30,43 @@ def my_artworks(request):
     })
 
 
-def home(request):
-    return render(request, 'marketplace/home.html')
+@login_required
+def create_artwork(request):
+    if request.method == 'POST':
+        form = ArtWorkCreateForm(request.POST,request.FILES)
+        if form.is_valid():
+            artwork = form.save(commit=False)
+            artwork.author = request.user
+            artwork.save()
+            return redirect('my_artworks')
+    else:
+        form = ArtWorkCreateForm()
+
+    return render(request, 'marketplace/create_artwork.html', {'form':form})
+
+def artwork_detail(request, artwork_id):
+    artwork = get_object_or_404(DigitalArtwork, id=artwork_id)
+    return render(request, 'marketplace/artwork_detail.html', {'artwork': artwork})
+
+@login_required
+def delete_artwork(request, artwork_id):
+    artwork = get_object_or_404(DigitalArtwork, id=artwork_id, author=request.user)
+    if request.method == 'POST':
+        artwork.delete()
+        return redirect('my_artworks')
+    return redirect('artwork_detail',artwork_id=artwork_id)
+
+@login_required
+def edit_artwork(request, artwork_id):
+    artwork = get_object_or_404(DigitalArtwork, id=artwork_id, author=request.user)
+
+    if request.method == 'POST':
+        form = ArtWorkCreateForm(request.POST, request.FILES, instance=artwork)
+        if form.is_valid():
+            form.save()
+            return redirect('artwork_detail', artwork_id=artwork.id)
+    else:
+        form = ArtWorkCreateForm(instance=artwork) 
+
+    return render(request, 'marketplace/edit_artwork.html', {'form': form, 'artwork': artwork})
+
