1ef9faf8f63625ea756ab18cba02d1137e799d51
Head-Shot-53 <denchikmashtack@gmail.com>
2025-03-28 13:42:24 +0200

    зареєстрував модель Category

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import DigitalArtwork
+from .models import DigitalArtwork, Category
 
 admin.site.register(DigitalArtwork)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent']
+    search_fields = ['name']
